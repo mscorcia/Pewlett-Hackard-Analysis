b76ae26923df2d9527bee0217ca62b88f7301026
mscorcia <mjscorcia@gmail.com>
2021-06-02 22:47:27 -0400

    Create Tables

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -33,4 +33,19 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
-SELECT * FROM departments;
+CREATE TABLE titles (
+  emp_no int NOT NULL,
+  title varchar(40) NOT NULL,
+  from_date date NOT NULL,
+  to_date date NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no,from_date));
+
+CREATE TABLE dept_emp (
+emp_no int NOT NULL,
+dept_no varchar(4) NOT NULL,
+from_date date NOT NULL,
+to_date date NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+PRIMARY KEY (emp_no,dept_no));
